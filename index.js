2c8f5f9a61bb6b40c8509f96ed1e5ce0d3960b8f
Immaculeta1 <immaculetapascaline@gmail.com>
2022-06-27 13:19:52 +0100

    task

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ mongoose
         console.log(err);
     });
 
+app.get("/pr", ()=> {
+    console.log("test is successfull!")
+});
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
